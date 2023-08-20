e82ea7241d783df37e9787d9c06af1731d1f65cf
jamessouth <jamesdanielsouth@gmail.com>
2023-08-20 01:13:13 -0500

    gravity

--- wasserzeichenbilder/bin/main.ml
+++ wasserzeichenbilder/bin/main.ml
@@ -1,31 +1,48 @@
 open Core
 
-let gravity =
-  let open Command.Param in
-  let grav dir =
-    let name = sprintf "-%s" dir in
-    let doc = sprintf " %s gravity" dir in
-    flag ~full_flag_required:() name no_arg ~doc
-    |> map ~f:(function false -> None | true -> Some dir)
-  in
+module Gravity : Command.Enumerable_stringable = struct
+  type t =
+    | NorthWest
+    | North
+    | NorthEast
+    | West
+    | Center
+    | East
+    | SouthWest
+    | South
+    | SouthEast
+  [@@deriving enumerate]
 
-  choose_one
-    [
-      grav "NorthWest";
-      grav "North";
-      grav "NorthEast";
-      grav "West";
-      grav "Center";
-      grav "East";
-      grav "SouthWest";
-      grav "South";
-      grav "SouthEast";
-    ]
-    ~if_nothing_chosen: (Default_to "Center")
+  let to_string = function
+    | NorthWest -> "NorthWest"
+    | North -> "North"
+    | NorthEast -> "NorthEast"
+    | West -> "West"
+    | Center -> "Center"
+    | East -> "East"
+    | SouthWest -> "SouthWest"
+    | South -> "South"
+    | SouthEast -> "SouthEast"
+end
+
+let gravity =
+  (module Gravity : Command.Enumerable_stringable with type t = Gravity.t)
+
+let gravities =
+  Command.Arg_type.enumerated ~accept_unique_prefixes:false
+    ~case_sensitive:false ~list_values_in_help:true gravity
 
 let () =
   Command_unix.run ~version:"1.0" ~build_info:"RWO"
     (Command.basic ~summary:"fib clock"
-       ~readme:(fun () -> "aosihaiu")
-       (let%map_open.Command ah = gravity in
-        fun () -> Stdlib.print_endline ah))
+       ~readme:(fun () -> "Enter ")
+       (let%map_open.Command gravity =
+          flag "-gravity"
+            (one_or_more_as_pair gravities)
+            ~doc:"string gravity"
+        in
+
+        fun () ->
+          let grav = fst gravity in
+
+          Stdlib.print_string (Gravity.to_string grav)))
