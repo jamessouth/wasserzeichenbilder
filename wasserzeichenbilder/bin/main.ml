a23550a88bebf8ad0894dc4ff8859438eac5e9b4
jamessouth <jamesdanielsouth@gmail.com>
2023-08-22 01:50:47 -0500

    input output flags

--- wasserzeichenbilder/bin/main.ml
+++ wasserzeichenbilder/bin/main.ml
@@ -21,26 +21,41 @@ let gravities =
     ~case_sensitive:false ~list_values_in_help:true
     (module Gravity : Command.Enumerable_stringable with type t = Gravity.t)
 
+let image_file =
+  Command.Arg_type.create (fun filename ->
+      match
+        ( Sys_unix.is_file filename,
+          List.exists [ "jpg"; "jpeg"; "png"; "webp"; "gif" ] ~f:(fun x ->
+              match snd (Filename.split_extension filename) with
+              | Some e -> String.( = ) e x
+              | None -> false) )
+      with
+      | `Yes, true -> filename
+      | `Yes, false -> failwith "Not an image file"
+      | `No, _ -> failwith "Not a regular file"
+      | `Unknown, _ -> failwith "Could not determine if this was a regular file")
+
 let () =
   Command_unix.run ~version:"1.0.0" ~build_info:"RWO"
     (Command.basic ~summary:"fib clock"
        ~readme:(fun () -> "Enter ")
        (let%map_open.Command gravity =
-          flag "-gravity"
-            (optional_with_default Gravity.Center gravities)
-            ~doc:"string gravity (default: Center)"
+          flag_optional_with_default_doc "-gravity" gravities
+            (fun x -> String.sexp_of_t (Gravity.to_string x))
+            ~default:Center ~doc:"string placement of the logo on the image"
         and font =
           flag_optional_with_default_doc "-font" string
             (fun x -> String.sexp_of_t x)
-            ~default:"FiraCode-Nerd-Font-Mono-Reg" ~doc:"string font"
+            ~default:"FiraCode-Nerd-Font-Mono-Reg"
+            ~doc:"string font containing the logo character"
         and pointsize =
           flag_optional_with_default_doc "-pointsize" int
             (fun x -> Int.sexp_of_t x)
-            ~default:300 ~doc:"int pointsize"
+            ~default:300 ~doc:"int pointsize of the logo character"
         and elevation =
           flag_optional_with_default_doc "-elevation" int
             (fun x -> Int.sexp_of_t x)
-            ~default:2 ~doc:"int elevation"
+            ~default:2 ~doc:"int height/depth of logo"
         and color1 =
           flag_optional_with_default_doc "-color1" string
             (fun x -> String.sexp_of_t x)
@@ -60,7 +75,13 @@ let () =
         and logo =
           flag_optional_with_default_doc "-logo" string
             (fun _ -> Sexp.Atom "_")
-            ~default:"" ~doc:"string logo (default:  (Linux penguin mascot))"
+            ~default:""
+            ~doc:"string logo character (default:  (Linux penguin))"
+        and image =
+          flag "-image" (required image_file)
+            ~doc:"string the input image (jpg, jpeg, png, webp, gif)"
+        and output =
+          flag "-output" (required string) ~doc:"string the output filename"
         in
 
         fun () ->
@@ -72,4 +93,7 @@ let () =
           Stdlib.print_string color2;
           Stdlib.print_string color3;
           Stdlib.print_string color4;
-          Stdlib.print_string logo))
+          Stdlib.print_string logo;
+          Stdlib.print_string image;
+          Stdlib.print_string output;
+          Stdlib.print_newline ()))
